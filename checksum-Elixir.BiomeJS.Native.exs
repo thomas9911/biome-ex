a5156038ce8d36ba3aff5d53c31cff38d6825158
Thomas Timmer <thomastimmer11@hotmail.com>
2023-11-28 17:44:42 +0100

    chore: add checksums

---
+++ checksum-Elixir.BiomeJS.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "biomejs_native-v0.1.0-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:779550032ea9552e8aeb9becfe1ffaef179138702f4550a121e0679212bf3e10",
+  "biomejs_native-v0.1.0-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:137c926a51ac75eb1105db58ee6f99d83d295980141f3484651a49ad19d77c98",
+  "libbiomejs_native-v0.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:332ca13d44bcac8d7f1a3a50dc0e90a9b0fb424eeda4db5fb28ea20e6a4bb71b",
+  "libbiomejs_native-v0.1.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:95ea3742849b650654417cdeec20caafce710564a492e77ebbe275afd2dd5a60",
+  "libbiomejs_native-v0.1.0-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:1f7f53cd8310067d50f5849044ea69c0db87a12bc2552597e9c80dad8091e302",
+  "libbiomejs_native-v0.1.0-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:2e3a942764e70c3c3856264d1747517e454ed275c44455b8facc0023481f59ea",
+  "libbiomejs_native-v0.1.0-nif-2.16-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:b824f6b33ac004556e60eebc02d898e941eb91173053806c9353892f938fa7d1",
+  "libbiomejs_native-v0.1.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:c9dced492855cb3f74dadd0186ef73fd9b66e2cffc932c9d4032ed907970537b",
+  "libbiomejs_native-v0.1.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0fca4a51c00b447521a6849038a7d7a1c6ea49a7b1d517e2d4ebcc9e5bdbbfd6",
+  "libbiomejs_native-v0.1.0-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:aa1e46fe89c15b5917d21a2ef0dafd77b8dc242abbdcc4b2c69e931e1cabcc5e",
+}
